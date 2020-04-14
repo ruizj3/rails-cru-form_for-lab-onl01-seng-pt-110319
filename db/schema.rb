bb14da0cd910d8a9ea3d072cdc450a297410f2f9
Joseph Ruiz <ruizj3@uw.edu>
2020-04-14 23:09:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200414224603) do
+ActiveRecord::Schema.define(version: 20200414224545) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
